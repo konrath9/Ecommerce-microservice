6cba278bfe62eba3c1952e43b18264ee22796986
Taynan Konrath <taynankonrath@gmail.com>
2024-06-03 20:03:08 -0300

    Creating product service for the web App

---
+++ Ecommerce/GeekShopping/GeekShopping.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.Web.Models;
+using System.Collections;
+
+namespace GeekShopping.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> FindAllProducts();
+        Task<ProductModel> FindProductByID(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProductById(long id);
+    }
+}
