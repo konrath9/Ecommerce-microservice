a4c9f3e3a5183de9085d73430fe002a46a42c1db
Taynan Konrath <taynankonrath@gmail.com>
2024-06-03 22:05:50 -0300

    Changing the layout and consuming Product API

--- Ecommerce/GeekShopping/GeekShopping.Web/Program.cs
+++ Ecommerce/GeekShopping/GeekShopping.Web/Program.cs
@@ -6,11 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
-
 builder.Services.AddHttpClient<IProductService, ProductService>(c =>
         c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"])
     );
+
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
